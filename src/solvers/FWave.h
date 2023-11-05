34c37f35d0e2c2157067f9cfd6c1f0c602359876
darafsa <marek1999@online.de>
2023-11-05 13:48:25 +0100

    header naming consistency

--- src/solvers/FWave.h
+++ src/solvers/FWave.h
@@ -28,9 +28,9 @@ class tsunami_lab::solvers::FWave {
 		 * @param in_stateRight state of the right side; 0: height, 1: momentum.
 		 * @param out_eigenvaluesRoe will be set to the Roe eigenvalues; 0: lambda^Roe_1, 1: lambda^Roe_2.
 		 */
-		static void computeEigenvalues(	real in_stateLeft[2], 
-													real in_stateRight[2], 
-													real out_eigenvaluesRoe[2] );
+		static void computeEigenvalues( real in_stateLeft[2], 
+												  real in_stateRight[2], 
+												  real out_eigenvaluesRoe[2] );
 
 		/**
 		 * @brief Calculates the flux function as a vaector.
@@ -38,7 +38,8 @@ class tsunami_lab::solvers::FWave {
 		 * @param in_state state of one cell; 0: height, 1: momentum.
 		 * @param out_flux will be set to the flux function values; 0: hu, 1: hu^2+0.5gh^2.
 		 */
-		static void flux(real in_state[2], real out_flux[2]);
+		static void flux( real in_state[2], 
+								real out_flux[2] );
 
 		/**
 		 * @brief Computes the inverted eigenmatrix.
@@ -46,8 +47,8 @@ class tsunami_lab::solvers::FWave {
 		 * @param in_eigenvalues Roe eigenvalues; 0: lambda^Roe_1, 1: lambda^Roe_2.
 		 * @param out_invertedEigenmatrix will be set to the inverted eigenmatrix; 0: eigenvector r1, 1: eigenvector r2.
 		 */
-		static void computeInvertedEigenmatrix(	real in_eigenvalues[2], 
-																real out_invertedEigenmatrix[2][2] );
+		static void computeInvertedEigenmatrix( real in_eigenvalues[2], 
+															 real out_invertedEigenmatrix[2][2] );
 
 		/**
 		 * @brief Computes the eigencoefficients.
@@ -57,10 +58,10 @@ class tsunami_lab::solvers::FWave {
 		 * @param in_invertedEigenmatrix inverted eigenmatrix; 0: eigenvector r1, 1: eigenvector r2.
 		 * @param out_eigencoefficients will be set to the eigencoefficients; 0: alpha_1, 1: alpha_2.
 		 */
-		static void computeEigencoefficients(	real in_stateLeft[2], 
-															real in_stateRight[2], 
-															real in_invertedEigenmatrix[2][2], 
-															real out_eigencoefficients[2] );
+		static void computeEigencoefficients( real in_stateLeft[2], 
+														  real in_stateRight[2], 
+														  real in_invertedEigenmatrix[2][2], 
+														  real out_eigencoefficients[2] );
 
 	public:
 		/**
@@ -71,7 +72,7 @@ class tsunami_lab::solvers::FWave {
 		 * @param out_netUpdateLeft will be set to the net-updates for the left side; 0: height, 1: momentum.
 		 * @param out_netUpdateRight will be set to the net-updates for the left side; 0: height, 1: momentum.
 		 */
-		static void netUpdates(	real in_stateLeft[2], 
+		static void netUpdates( real in_stateLeft[2], 
 	 									real in_stateRight[2], 
 										real out_netUpdateLeft[2], 
 										real out_netUpdateRight[2] );
