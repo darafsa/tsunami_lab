841805d185972057377c987b9fe7ebc691e799ab
darafsa <marek1999@online.de>
2023-11-16 13:49:46 +0100

    removed bathymetry, height addition

--- src/io/Csv.cpp
+++ src/io/Csv.cpp
@@ -34,8 +34,7 @@ void tsunami_lab::io::Csv::write( t_real               i_dxy,
 
       // write data
       io_stream << l_posX << "," << l_posY;
-      if( i_h  != nullptr && i_b  != nullptr ) io_stream << "," << i_h[l_ix]+i_b[l_ix];
-      else if( i_h  != nullptr ) io_stream << "," << i_h[l_id];
+      if( i_h  != nullptr ) io_stream << "," << i_h[l_id];
       if( i_b  != nullptr ) io_stream << "," << i_b[l_ix];
       if( i_hu != nullptr ) io_stream << "," << i_hu[l_id];
       if( i_hv != nullptr ) io_stream << "," << i_hv[l_id];
