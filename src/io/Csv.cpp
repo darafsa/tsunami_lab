8264c0bb46f4810e9bf9320fb184c6cc36e3f712
darafsa <marek1999@online.de>
2023-11-19 21:57:56 +0100

    csv id fix for bathymetry

--- src/io/Csv.cpp
+++ src/io/Csv.cpp
@@ -39,7 +39,7 @@ void tsunami_lab::io::Csv::write( t_real               i_dxy,
       // write data
       io_stream << l_posX << "," << l_posY;
       if( i_h  != nullptr ) io_stream << "," << i_h[l_id];
-      if( i_b  != nullptr ) io_stream << "," << i_b[l_ix];
+      if( i_b  != nullptr ) io_stream << "," << i_b[l_id];
       if( i_hu != nullptr ) io_stream << "," << i_hu[l_id];
       if( i_hv != nullptr ) io_stream << "," << i_hv[l_id];
       io_stream << "\n";
